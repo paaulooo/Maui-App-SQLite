65554312ac2bbd7d48f4128b1476d3909007118c
paaulooo <paulorobertofer98@gmail.com>
2026-03-21 11:41:57 -0300

    validação de inserçao de produtos

--- MauiAppSQLite/Models/Product.cs
+++ MauiAppSQLite/Models/Product.cs
@@ -9,11 +9,15 @@ namespace MauiAppSQLite.Models
 {
     public class Product
     {
+        string _name;
+        decimal _price;
+        int _quantity;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        public string Name { get => _name; set { if (value == null) throw new Exception("Por favor preencha o nome!"); _name = value; }  }
+        public decimal Price { get => _price; set { if (value <= 0) throw new Exception("Por favor preencha o preço!"); _price = value; } }
+        public int Quantity { get => _quantity; set { if (value <= 0 ) throw new Exception("Por favor preencha a quantidade!"); _quantity = value; } }
         public decimal TotalPrice { get => Price * Quantity; }
     }
 }
